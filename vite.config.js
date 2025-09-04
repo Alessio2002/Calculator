165755edc431b7bfc967a486525f283e96976342
Alessio2002 <alessio.zanasi02@gmail.com>
2025-09-04 10:03:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Calculator/",  // Base URL for GitHub Pages
+  base: "/Calculator/",  // exactly your repo name with slashes
   plugins: [react()],
 });
